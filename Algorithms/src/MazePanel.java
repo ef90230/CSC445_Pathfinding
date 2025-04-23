59d421f1539d023a783a1f7de6f88d754884df04
washford226 <washford@murraystate.edu>
2025-04-23 13:27:33 -0500

    Started on the display

---
+++ Algorithms/src/MazePanel.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+public class MazePanel extends JPanel {
+    private final List<Node> allNodes;
+    private final Node start, goal;
+    private final int rows, cols;
+    private List<Node> path;
+    private final boolean[][] obstacles; // 2D array to track obstacles
+
+    public MazePanel(List<Node> allNodes, Node start, Node goal, int rows, int cols) {
+        this.allNodes = allNodes;
+        this.start = start;
+        this.goal = goal;
+        this.rows = rows;
+        this.cols = cols;
+        this.obstacles = new boolean[rows][cols];
+
+        // Add mouse listener to toggle obstacles
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int cellWidth = getWidth() / cols;
+                int cellHeight = getHeight() / rows;
+                int x = e.getX() / cellWidth;
+                int y = e.getY() / cellHeight;
+
+                // Toggle obstacle, but don't allow start or goal to be obstacles
+                if (!(start.x == x && start.y == y) && !(goal.x == x && goal.y == y)) {
+                    obstacles[x][y] = !obstacles[x][y];
+                    repaint();
+                }
+            }
+        });
+    }
+
+    public void setPath(List<Node> path) {
+        this.path = path;
+        repaint(); // Repaint the panel to reflect the updated path
+    }
+
+    public boolean isObstacle(int x, int y) {
+        if (x < 0 || x >= rows || y < 0 || y >= cols) {
+            return true; // Treat out-of-bounds as obstacles
+        }
+        return obstacles[x][y];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int cellWidth = getWidth() / cols;
+        int cellHeight = getHeight() / rows;
+
+        // Draw grid
+        for (Node node : allNodes) {
+            int x = node.x * cellWidth;
+            int y = node.y * cellHeight;
+
+            // Draw obstacles
+            if (obstacles[node.x][node.y]) {
+                g.setColor(Color.BLACK);
+                g.fillRect(x, y, cellWidth, cellHeight);
+            } else {
+                g.setColor(Color.LIGHT_GRAY);
+                g.fillRect(x, y, cellWidth, cellHeight);
+            }
+
+            g.setColor(Color.BLACK);
+            g.drawRect(x, y, cellWidth, cellHeight);
+        }
+
+        // Highlight start and goal
+        g.setColor(Color.GREEN);
+        g.fillRect(start.x * cellWidth, start.y * cellHeight, cellWidth, cellHeight);
+        g.setColor(Color.BLUE);
+        g.fillRect(goal.x * cellWidth, goal.y * cellHeight, cellWidth, cellHeight);
+
+        // Highlight path
+        if (path != null) {
+            g.setColor(Color.RED);
+            for (Node node : path) {
+                int x = node.x * cellWidth;
+                int y = node.y * cellHeight;
+                g.fillRect(x, y, cellWidth, cellHeight);
+            }
+        }
+    }
+}
--- Algorithms/src/Node.java
+++ Algorithms/src/Node.java
@@ -2,23 +2,27 @@
 //TODO: Add ability to add nodes, obstacles
 
 public class Node implements Comparable<Node> {
-    int x, y;
-    int g, h, f;
-    Node parent;
+    int x, y; // Coordinates of the node
+    int g, h, f; // Costs for A* algorithm
+    Node parent; // Reference to the parent node for path reconstruction
 
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
+        this.g = Integer.MAX_VALUE; // Initialize g to a large value (infinity)
+        this.h = 0;
+        this.f = Integer.MAX_VALUE; // Initialize f to a large value (infinity)
+        this.parent = null;
     }
 
     public int calculateF() {
         this.f = this.g + this.h;
         return this.f;
     }
-    
+
     @Override
     public int compareTo(Node other) {
-        return Integer.compare(this.f, other.f);
+        return Integer.compare(this.f, other.f); // Compare nodes based on their f value
     }
 
     @Override
@@ -26,11 +30,16 @@ public class Node implements Comparable<Node> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Node node = (Node) o;
-        return x == node.x && y == node.y;
+        return x == node.x && y == node.y; // Nodes are equal if their coordinates match
     }
 
     @Override
     public int hashCode() {
-        return 31 * x + y;
+        return 31 * x + y; // Generate a unique hash code based on coordinates
+    }
+
+    @Override
+    public String toString() {
+        return "Node(" + x + ", " + y + ")";
     }
 }
